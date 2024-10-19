a32a2de124678b35d2497d0f02d47d83a63b7e95
SamuelB4 <128752342+SamuelB4@users.noreply.github.com>
2024-10-19 09:56:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let text = document.getElementsByClassName("text");
-let h1 = document.getElementsByClassName("h1");
-let h3 = document.getElementsByClassName("h3");
-
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-    text.style.fontSize = "6px";
-    h1.style.fontSize = "16px";
-    h3.style.fontSize = "12px";
-  }
+    document.querySelectorAll("p").style.fontSize = "6px";
+    document.querySelectorAll("h1").style.fontSize = "16px";
+    document.querySelectorAll("h3").style.fontSize = "12px";
+}
