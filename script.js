da9fa48d1792187d8cae43ff775523626fd92413
SamuelB4 <128752342+SamuelB4@users.noreply.github.com>
2024-10-19 13:52:17 +0100

    Add files via upload

--- script.js
+++
@@ -1,6 +0,0 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    // true for mobile device
-    document.querySelectorAll(".text").style.fontSize = "4px";
-    document.querySelectorAll(".h1").style.fontSize = "12px";
-    document.querySelectorAll(".h3").style.fontSize = "8px";
-}
